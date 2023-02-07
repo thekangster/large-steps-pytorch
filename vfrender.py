e3687395533d175786fffc1985dcb97982e671eb
Kevin Kang <kevinkang513@gmail.com>
2023-02-06 21:04:53 -0800

    i dont know

--- vfrender.py
+++ vfrender.py
@@ -15,6 +15,11 @@ mi.set_variant('llvm_ad_rgb')
 
 scene_params = mi.load_file(filepath)#, res=128)#, integrator='prb')
 image_ref = mi.render(scene_params, spp=512)
+'''
+plt.axis("off")
+plt.imshow(mi.util.convert_to_bitmap(image_ref));
+plt.show()
+'''
 
 # get largesteps scene params
 scene_large = load_scene(filepath)
@@ -45,6 +50,19 @@ index = dr.arange(mi.UInt32, N - 1)
 face_indices = mi.Vector3u(N - 1, (index + 1) % (N - 2), index % (N - 2))
 #print("len(face_indices) = ", len(face_indices))
 
+# generate face normals ??
+nref = n_ref.tolist()
+x2 = dr.zeros(mi.Float, len(v_ref))
+y2 = dr.zeros(mi.Float, len(v_ref))
+z2 = dr.zeros(mi.Float, len(v_ref))
+idx = 0
+for i in vref:
+    x2[idx] = i[0]
+    y2[idx] = i[1]
+    z2[idx] = i[2]
+    idx += 1
+face_norms = mi.Point3f(x2, y2, z2)
+
 # create mesh
 mesh = mi.Mesh(
     "mymesh", 
@@ -56,8 +74,10 @@ mesh = mi.Mesh(
 )
 
 mesh_params = mi.traverse(mesh)
+print(mesh_params)
 mesh_params['vertex_positions'] = dr.ravel(vertex_pos)
 mesh_params['faces'] = dr.ravel(face_indices)
+#mesh_params['vertex_normals'] = dr.ravel(face_norms)
 print(mesh_params.update())
 
 scene = mi.load_dict({
@@ -67,7 +87,7 @@ scene = mi.load_dict({
     "sensor": {
         "type": "perspective",
         "to_world": mi.ScalarTransform4f.look_at(
-            origin=[0, -5, 10], target=[0, 0, 0], up=[0, 0, 1]
+            origin=[0, -1, 10], target=[0, 0, 0], up=[0, 0, 1]
         ),
     },
     "mymesh": mesh,
@@ -78,4 +98,6 @@ img = mi.render(scene)
 plt.axis("off")
 plt.imshow(mi.util.convert_to_bitmap(img));
 plt.show()
+
+mesh.write_ply("mymesh.ply")
 
